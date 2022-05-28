b4a9f22652f8e182ab6344c0eb88454de300e037
Danilo <daconnas.dcn@gmail.com>
2022-05-28 12:43:31 -0300

    congigurando banco

--- backend/settings.py
+++ backend/settings.py
@@ -79,7 +79,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR + "/db.sqlite3",
     }
 }
 
